d713de07799dc0c8170410fd72b47fec7055b84a
bnamgung@gmail.com <bnamgung@gmail.com>
2017-03-13 19:31:59 -0700

    worked on js

--- busmall/app.js
+++ busmall/app.js
@@ -12,21 +12,133 @@ function surveyImages(name, filePath){
 
 //generate random numbers
 function randomArray (min, max){
-  for(var i = 0; i < 3; i++){
-    var displayArray= [];
-    displayArray.push(Math.floor(Math.random() * (max - min)) + min);
+  var getThreeIndex = [];
+  for (var i = 0; i < 3; i++) {
+    getThreeIndex.push(Math.floor(Math.random() * (max - min)) + min);
   }
-  while (displayArray[0] === displayArray[1] ||
-    displayArray[1] === displayArray[2] ||
-    displayArray[0] === displayArray[2]){
-      displayArray.splice(1,2); //splice = changes content of an array, adding new elements while removing old.
-      displayArray.push(Math.floor(Math.random() * (max - min)) + min);
-      displayArray.push(Math.floor(Math.random() * (max - min)) + min);
-    }
-    return displayArray;
+  while (getThreeIndex[0] === getThreeIndex[1] || getThreeIndex[1] === getThreeIndex[2] || getThreeIndex[0] === getThreeIndex[2]) {
+    getThreeIndex.splice(1, 2);
+    getThreeIndex.push(Math.floor(Math.random() * (max - min)) + min);
+    getThreeIndex.push(Math.floor(Math.random() * (max - min)) + min);
   }
+  return getThreeIndex;
+}
 
   //to clear elements
   function clearBox(elementId){
-    document.getElementById(elementId).innerHTML = "";
+    document.getElementById(elementId).innerHTML = '';
   }
+
+//count clicks
+function addClicks(id){
+  for(var i = 0; i < surveyImagesArray.length; i++){
+    if (surveyImagesArray[i].name == id){
+      surveyImagesArray[i].timesClicked ++;
+      totalClicks ++;
+      console.log('total clicks: ' + totalClicks);
+      break;
+    }
+  }
+}
+
+//to render three random images
+function renderImages(){
+  key= randomNoArray(0, surveyImagesArray.length);
+  for(var i = 0; i < 3; i++){
+    randomObject= surveyImagesArray[key[i]];
+    console.log('randomObject', randomObject);
+
+    elBody= document.getElementById('marketResearch');
+    for(var i = 0; i < 3; i++){
+      randomObject = surveyImagesArray[getThreeIndex[i]];
+    elImg.setAttribute('class', 'survey-display')
+    elImg.setAttribute('src', randomObject.filePath);
+    elImg.setAttribute('id', randomObject.name);
+    elBody.appendChild(elImg);
+    randomObject.timesRendered++;
+  }
+eventListenerClickImage();
+}
+
+//event handler
+function handleImageClick(event){
+  var imgEl= event.target;
+  var idEl= imgEl.id;
+  addClicks(idEl);
+  clearBox('marketResearch');
+  if (totalClicks < 15){
+  renderImages();
+}else{
+  createButton('results', 'Click for results!');
+  eventListenerResultsButton();
+}
+}
+
+function eventListenerClickImage(){
+  var surveyDisplay= document.getElementByClassName('survey-display');
+  for(var i = 0; i < surveyDisplay.length; i++){
+    surveyDisplay[i].addEventListener('click', handleImageClick);
+  }
+}
+
+function eventListenerResultsButton(){
+  var button= document.getElementById('results');
+  button.addEventListener('click', generateGraph);
+}
+
+function BarChartData(){
+  this.labels= [];
+  this.datasets= [];
+}
+
+function BarDataSet(labelName, color){
+  this.label= labelName;
+  this.fillColor= color;
+  this.strokeColor= color;
+  this.highlightFill= color;
+  this.highlightStroke= color;
+  this.data= [];
+}
+
+BarDataSet.prototype.setFields= function (inputArray, field){
+  for(var i = 0; i < inputArray.length; i++){
+    this.data.push(inputArray[i][field]);
+  }
+};
+
+
+}
+
+function eventListener(){
+  var surveyDisplay= document.getElementByClassName('survey-display');
+  for(var i = 0; i < surveyDisplay.length; i++){
+    surveyDisplay[i].addEventListener('click', handleImageClick);
+  }
+}
+
+//render Images & Event Listener function
+renderImages();
+eventListener();
+
+
+//survey images (I'm sure I can factor this)
+var bagImg = new SurveyImages('bag', 'pic/bag.jpg');
+var bananaImg = new SurveyImages('banana', 'pic/banana.jpg');
+var bathroomImg = new SurveyImages('bathroom', 'pic/bathroom.jpg');
+var bootsImg = new SurveyImages('boots', 'pic/boots.jpg');
+var breakfastImg = new SurveyImages('breakfast', 'pic/breakfast.jpg');
+var bubblegumImg = new SurveyImages('bubblegum', 'pic/bubblegum.jpg');
+var chairImg = new SurveyImages('chair', 'pic/chair.jpg');
+var cthulhuImg = new SurveyImages('cthulhu', 'pic/cthulhu.jpg');
+var dogduckImg = new SurveyImages('dog-duck', 'pic/dog-duck.jpg');
+var dragonImg = new SurveyImages('dragon', 'pic/dragon.jpg');
+var penImg = new SurveyImages('pen', 'pic/pen.jpg');
+var petsweepImg = new SurveyImages('pet-sweep', 'pic/pet-sweep.jpg');
+var scissorsImg = new SurveyImages('scissors', 'pic/scissors.jpg');
+var sharkImg = new SurveyImages('shark', 'pic/shark.jpg');
+var sweepImg = new SurveyImages('sweep', 'pic/sweep.png');
+var tauntaunImg = new SurveyImages('tauntaun', 'pic/tauntaun.jpg');
+var unicornImg = new SurveyImages('unicorn', 'pic/unicorn.jpg');
+var usbImg = new SurveyImages('usb', 'pic/usb.gif');
+var watercanImg = new SurveyImages('water-can', 'pic/water-can.jpg');
+var wineglassImg = new SurveyImages('wine-glass', 'pic/wine-glass.jpg');
